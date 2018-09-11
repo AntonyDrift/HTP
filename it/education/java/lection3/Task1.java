05859123c13faa0873333de6fe2faf2f5a5d519c
Andrewyuraga <35347458+Andrewyuraga@users.noreply.github.com>
2018-09-11 11:46:01 +0300

    Create Task1.java

---
+++ it/education/java/lection3/Task1.java
@@ -0,0 +1,17 @@
+/*
+ Напишите программу, которая печатает массив сначала в обычном порядке, затем в обратном.
+ */
+ 
+public class Task1 {
+    public static void main(String[] args) {
+        int mas[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            mas[i] = (int) (Math.random() * 10 + 1);
+            System.out.println("Цифра " + i + " " + mas[i]);
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println("Значение " + i + " " + mas[i]);
+        }
+    }
+}
